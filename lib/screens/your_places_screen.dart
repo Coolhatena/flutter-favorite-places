a65a53152b498231d22588641a5a9233539b45dc
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-12-07 18:39:52 -0800

    Add persistent data loading

--- lib/screens/your_places_screen.dart
+++ lib/screens/your_places_screen.dart
@@ -4,11 +4,34 @@ import 'package:favorite_places/screens/place_details.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class YourPlacesScreen extends ConsumerWidget {
+class YourPlacesScreen extends ConsumerStatefulWidget {
   const YourPlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<YourPlacesScreen> createState() {
+    return _YourPlacesScreenState();
+  }
+}
+
+class _YourPlacesScreenState extends ConsumerState<YourPlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(placesProvider.notifier).loadPlaces();
+  }
+
+  void addPlace() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const AddPlaceScreen(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final yourPlaces = ref.watch(placesProvider);
 
     Widget content = yourPlaces.isEmpty
@@ -23,45 +46,51 @@ class YourPlacesScreen extends ConsumerWidget {
           )
         : Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ListView.builder(
-              itemCount: yourPlaces.length,
-              itemBuilder: (context, index) => ListTile(
-                leading: CircleAvatar(
-                  radius: 26,
-                  backgroundImage: FileImage(yourPlaces[index].image),
-                ),
-                title: Text(
-                  yourPlaces[index].name,
-                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                        color: Theme.of(context).colorScheme.onSurface,
-                      ),
-                ),
-                subtitle: Text(
-                  yourPlaces[index].location.address,
-                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                        color: Theme.of(context).colorScheme.onSurface,
-                      ),
-                ),
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => PlaceDetails(
-                        place: yourPlaces[index],
+            child: FutureBuilder(
+              future: _placesFuture,
+              builder: (context, snapshot) => snapshot.connectionState ==
+                      ConnectionState.waiting
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : ListView.builder(
+                      itemCount: yourPlaces.length,
+                      itemBuilder: (context, index) => ListTile(
+                        leading: CircleAvatar(
+                          radius: 26,
+                          backgroundImage: FileImage(yourPlaces[index].image),
+                        ),
+                        title: Text(
+                          yourPlaces[index].name,
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleMedium!
+                              .copyWith(
+                                color: Theme.of(context).colorScheme.onSurface,
+                              ),
+                        ),
+                        subtitle: Text(
+                          yourPlaces[index].location.address,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodySmall!
+                              .copyWith(
+                                color: Theme.of(context).colorScheme.onSurface,
+                              ),
+                        ),
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => PlaceDetails(
+                                place: yourPlaces[index],
+                              ),
+                            ),
+                          );
+                        },
                       ),
                     ),
-                  );
-                },
-              ),
             ),
           );
-
-    void addPlace() {
-      Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) => const AddPlaceScreen(),
-        ),
-      );
-    }
 
     return Scaffold(
       appBar: AppBar(
